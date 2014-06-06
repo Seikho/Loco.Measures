e82784ec650a7974af7ad448d6ff2552c3f49974
Carl <carl@carlossus.com>
2014-06-07 00:17:23 +0800

    added BlankFiller.cs to project

---
+++ LocoDataExtractor/Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace LocoDataExtractor
+{
+    public static class Extensions
+    {
+        public static string GetClassName(this object obj)
+        {
+            var split = obj.GetType().ToString().Split('.');
+            return split[split.Length - 1];
+        }
+    }
+}
---
+++ LocoDataExtractor/Processors/BlankFiller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LocoDataExtractor.Processors
+{
+    public class BlankFiller : Processor
+    {
+        protected DateTime Pred;
+        protected DateTime Succ;
+        protected DateTime Curr;
+        protected string PredByte;
+        protected bool ReadFirst = false;
+        protected string Line;
+        public BlankFiller(string fileLocation, string newFile) : base(fileLocation, newFile)
+        {
+            ReadFirst = false;
+            Pred = new DateTime();
+            Succ = new DateTime();
+            PredByte = ""; // previous sample byte
+        }
+
+        protected override void Execute()
+        {
+            while ((Line = Original.ReadLine()) != null)
+            {
+                if (Line.Length < 5) continue;
+                var split = Line.Split(' ');
+                if (!ReadFirst) GenerateInitialValues(split);
+                else Curr = GetTime(Line);
+                if (Pred == Curr) continue;
+                if (Succ != Curr) // we're missing a second or two. fill in the gap(s).
+                {
+                    while (Succ != Curr)
+                    {
+                        Target.WriteLine("{0} {1} {2} {3}", Succ.ToShortDateString(), Succ.ToLongTimeString(), split[3],
+                            PredByte);
+                        Succ = Succ.AddSeconds(1);
+                    }
+                }
+                // we do this regardless.
+                Succ = Curr.AddSeconds(1);
+                Pred = Curr;
+                PredByte = split[4];
+                Target.WriteLine(Line);
+            }
+        }
+
+        private void GenerateInitialValues(string[] split)
+        {
+            ReadFirst = true;
+            PredByte = split[4];
+            Curr = GetTime(Line);
+            Pred = Curr.AddSeconds(-1);
+            Succ = Curr;
+        }
+    }
+}
